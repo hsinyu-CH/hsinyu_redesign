5812a2c59c4bd2835cef750098fc4594f049498c
hsinyu-CH <joanna2965@gmail.com>
2025-08-12 16:36:59 +0800

    圖片區

---
+++ js/js.js
@@ -0,0 +1,28 @@
+document.querySelectorAll('.card-scroll').forEach(scrollContainer => {
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    scrollContainer.addEventListener('mousedown', e => {
+        isDown = true;
+        scrollContainer.classList.add('active');
+        startX = e.pageX - scrollContainer.offsetLeft;
+        scrollLeft = scrollContainer.scrollLeft;
+    });
+
+    scrollContainer.addEventListener('mouseleave', () => {
+        isDown = false;
+    });
+
+    scrollContainer.addEventListener('mouseup', () => {
+        isDown = false;
+    });
+
+    scrollContainer.addEventListener('mousemove', e => {
+        if (!isDown) return;
+        e.preventDefault();
+        const x = e.pageX - scrollContainer.offsetLeft;
+        const walk = (x - startX) * 1; // 拖曳速度倍率
+        scrollContainer.scrollLeft = scrollLeft - walk;
+    });
+});
